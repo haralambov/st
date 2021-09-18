af6fe2abeee72d5351e313be8d1d524d18b70233
Zlatomir Haralambov <z.haralambov@gmail.com>
2021-09-18 22:28:50 +0300

    Adds new terminal in current directory patch

--- config.def.h
+++ config.def.h
@@ -194,6 +194,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_u,           copyurl,        {.i =  0} },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
